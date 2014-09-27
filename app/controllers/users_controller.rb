a1cf1a64d31de631336fa762b76e54a5ce851d91
Robert Petrowsky <bobpetrowsky@gmail.com>
2014-09-27 17:53:22 -0400

    changed some stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,5 @@
 class UsersController < ApplicationController
   def index
-  # @graph = Koala::Facebook::API.new(current_user.token, ENV['FACEBOOK_SECRET'])
-
-  # @friends = @graph.get_connections("me", "friends")
   end
 
 end
